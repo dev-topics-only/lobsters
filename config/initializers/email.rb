43b6913247f0e66f68eaca81c33740f2cfb0342b
pipecraft <pipecraft.net@gmail.com>
2022-12-20 17:17:54 +0800

    [DTO] Fix send email

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,8 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address => ENV.fetch("SMTP_HOST", "127.0.0.1"),
   :port => Integer(ENV.fetch("SMTP_PORT", 25)),
-  :domain => Rails.application.domain,
+  :domain => ENV.fetch("SMTP_DOMAIN", Rails.application.domain),
   :enable_starttls_auto => (ENV["SMTP_STARTTLS_AUTO"] == "true"),
   :user_name => ENV.fetch("SMTP_USERNAME", ""),
   :password => ENV.fetch("SMTP_PASSWORD", ""),
+  :ssl => (ENV["SMTP_SSL"] == "true"),
 }
